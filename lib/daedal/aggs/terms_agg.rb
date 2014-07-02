ff6df276185add976e9786deb2d0687b73a22c69
Jason See <jason@jasonsee.me>
2014-07-02 10:47:39 -0700

    completed terms_agg (untested)

--- lib/daedal/aggs/terms_agg.rb
+++ lib/daedal/aggs/terms_agg.rb
@@ -12,11 +12,14 @@ module Daedal
       attribute :min_doc_count, Integer, required: false
       attribute :order, Hash[String => String], required: false
 
+      #include/exclude must present for include/exclude_flags
       attribute :include, String, required: false
       attribute :include_flags, Daedal::Attributes::PatternFlag, required: false
 
       attribute :exclude, String, required: false
       attribute :exclude_flags, Daedal::Attributes::PatternFlag, required: false
+
+      attribute :size, Integer, required: false
 
 
       def initialize(options={})
@@ -24,17 +27,25 @@ module Daedal
 
         if field.nil? && script.nil?
           raise "Must give a field or a script"
+        elsif exclude_flags && exclude.nil?
+          raise "Please include exclude field"
+        elsif include_flags && include.nil?
+          raise "Please include include field"
         end
       end
 
       def to_hash
-        result = {name => {stats: {}}}
+        result = {name => { terms: {} }}
 
-        if field
-          result[name][:stats] = {field: field}
-        else
-          result[name][:stats] = {script: script}
-        end
+        result[name][:terms][:field] = field if field
+        result[name][:terms][:script] = script if script
+        result[name][:terms][:min_doc_count] = min_doc_count if min_doc_count
+        result[name][:terms][:order] = order if order
+        result[name][:terms][:include] = include if include
+        result[name][:terms][:include][:flags] = include if include_flags
+        result[name][:terms][:exclude] = exclude if exclude
+        result[name][:terms][:exclude][:flags] = exclude if exclude_flags
+        result[name][:terms][:size] = size if size
 
         result
       end
